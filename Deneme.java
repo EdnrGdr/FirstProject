2cf308d75ae23691b6810cf3ec298711db1847b9
edanu <edanurgider94@hotmail.com>
2024-03-26 00:33:46 +0300

    26.03.2024 - 00:33

---
+++ Deneme.java
@@ -0,0 +1,14 @@
+public class Deneme {
+
+    public static void main (String[] args) {  // main metodu olmadan calısmaz.
+
+        System.out.println("Merhaba Java");
+        System.out.println("Hoşgeldiniz..");
+        System.out.print("Edanur");
+        System.out.print("Gideros..");
+
+
+    }
+
+
+}
